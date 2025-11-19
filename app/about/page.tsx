e48bec4ba7fb5a55d114bdd82deb7b50ea487bfb
Simon Tyler <simontyler@Mac-Studio.local>
2025-11-19 20:48:46 +0000

    footer etc

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
+import { PageTitle } from "@/components/typography";
+
 export default function AboutPage() {
   return (
-    <main className="mx-auto  px-4 lg:px-8 py-10 lg:py-16">
-      <h1 className="text-sm md:text-base tracking-[0.18em] uppercase text-neutral-500 font-[600]">
-        About
-      </h1>
-      <div className="mt-4 max-w-3xl space-y-4 text-[15px] md:text-[18px] leading-relaxed text-neutral-900">
+    <main className="mx-auto px-4 lg:px-8 py-10 lg:py-16">
+      <header className="max-w-3xl space-y-4 mb-8 lg:mb-12">
+        <PageTitle>About Elxsis</PageTitle>
+        <p className="text-[15px] md:text-[18px] leading-relaxed text-neutral-900">
         <p>
           Elxsis is the experimental arm of designer and artist Simon Tyler’s
           practice – a studio for work at the intersection of science,
@@ -15,8 +16,8 @@ export default function AboutPage() {
           ranging from large-format prints and motion pieces to interactive
           visualisations and research-led collaborations with scientists,
           mathematicians, museums, and technology organisations.
-        </p>
-      </div>
+          </p>
+      </header>
     </main>
   );
 }
